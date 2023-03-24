7d026b43b85b76c228db31398c93b4a053acc693
GrumpyMonk6996 <GrumpyMonk6996@gmail.com>
2023-03-24 15:07:17 +0100

    Login page and register.

--- layout/menu.php
+++ layout/menu.php
@@ -1,7 +1,8 @@
 <!-- A menu sáv az odalon -->
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="navBar">
-    <a href="http://localhost/0.3.0/index.php"><span class="navbar-brand mb-0 h1">Place holder for Company Name</span>   </a>
-    <a href="http://localhost/0.3.0/layout/shop.php" class="navbar-item">Shop</a>
+    <a href="index.php"><span class="navbar-brand mb-0 h1">Place holder for Company Name</span>   </a>
+    <a href="layout/shop.php" class="navbar-item">Shop</a>
+    <a href="login/register.php" class="navbar-item">Bejelentkezés</a>
 </nav> 
 <!--asd-->
