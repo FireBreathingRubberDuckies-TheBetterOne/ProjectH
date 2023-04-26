071fe5e7676c9be6d3ca43a91f6f6f67f2919b21
N0rbi03 <tothnorbi2003@gmail.com>
2023-04-26 16:47:26 +0200

    kep próbálkozás

--- backend/classes/productLoader.class.php
+++ backend/classes/productLoader.class.php
@@ -14,16 +14,41 @@ class ProducLoader extends database{
         $product = "<div class=\"container\"><div class=\"row\">";
         // $tt = null;
         if($result->num_rows>0){
-            while($row = $result->fetch_assoc()){
+            while($row = $result->fetch_assoc()){ 
                 if($teszt1 == 3){
+                    if($row["kep"]==null)
+                    {
+                        $product .= "
+                        <a href=\"https://localhost/ProjectH/view/product.php?idTermek=$row[isokod]\" class=\"col-sm-12 col-md-6 col-lg-3 igen m-1\"><div><img src=\"../pictures/alap.jpg\" class=\"col-sm-12 col-md-6 col-lg-3 \" alt=\"$row[termnev]\">$row[termnev]</div></a>
+                        </div><div class=\"row\">";
+                    }
+                    else
+                    {
+
+                    
                     $product .= "
-                    <a href=\"https://localhost/ProjectH/view/product.php?idTermek=$row[isokod]\" class=\"col-sm-12 col-md-6 col-lg-3 igen m-1\"><div>$row[termnev]</div></a>
+                    <a href=\"https://localhost/ProjectH/view/product.php?idTermek=$row[isokod]\" class=\"col-sm-12 col-md-6 col-lg-3 igen m-1\"><div><img src=\"../pictures/$row[kep]\" class=\"col-sm-12 col-md-6 col-lg-3 \" alt=\"$row[termnev]\">$row[termnev]</div></a>
                     </div><div class=\"row\">";
+                    }
                     $teszt1 = 0;
                     // $tt = $row['nev'];
                     }
                 else{
-                    $product .= "<a href=\"https://localhost/ProjectH/view/product.php?idTermek=$row[isokod]\" class=\"col-sm-12 col-md-6 col-lg-3 igen m-1\"><div>$row[termnev]</div></a>";
+                    if($row["kep"]==null)
+                    {
+                        $product .= "
+                        <a href=\"https://localhost/ProjectH/view/product.php?idTermek=$row[isokod]\" class=\"col-sm-12 col-md-6 col-lg-3 igen m-1\"><div><img src=\"../pictures/alap.jpg\" class=\"col-sm-12 col-md-6 col-lg-3 \" alt=\"$row[termnev]\">$row[termnev]</div></a>
+                        </div><div class=\"row\">";
+                    }
+                    else
+                    {
+
+                    
+                    $product .= "
+                    <a href=\"https://localhost/ProjectH/view/product.php?idTermek=$row[isokod]\" class=\"col-sm-12 col-md-6 col-lg-3 igen m-1\"><div><img src=\"../pictures/$row[kep]\"class=\"col-sm-12 col-md-6 col-lg-3 \" alt=\"$row[termnev]\">$row[termnev]</div></a>
+                    </div><div class=\"row\">";
+                    }
+                    
                     // $tt = $row['nev'];
                     $teszt1++;
                 }
